c4360d179560cd2c6964a382d5aad27f366214de
Evan Astle <starphys@gmail.com>
2022-03-17 16:57:37 -0700

    Added flash, seems to match prompt for Q2

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
-from flask import render_template
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "you-will-never-guess"
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,16 @@
-from flask import render_template
+from flask import render_template, request, flash
 from app import app
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
     name = "Lisa"
     city_names = ["Paris", "London", "Rome", "Tahiti"]
-    return render_template("home.html", name=name, city_names=city_names)
+
+    if request.method == "GET":
+        return render_template("home.html", name=name, city_names=city_names)
+
+    if request.method == "POST":
+        city = request.form["inputcity"]
+        flash(city)
+        return render_template("home.html", name=name, city_names=city_names)
