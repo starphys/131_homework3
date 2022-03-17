ffaaef84deab35f3b339ce4805d678a11f062d14
Evan Astle <starphys@gmail.com>
2022-03-17 15:33:07 -0700

    Header correct

--- home.py
+++ home.py
@@ -5,7 +5,9 @@ myapp_obj = Flask(__name__)
 
 @myapp_obj.route("/")
 def home():
-    return """HTML"""
+    name = "Lisa"
+    city_names = ["Paris", "London", "Rome", "Tahiti"]
+    return f"""<h1>Welcome {name}</h1>"""
 
 
 myapp_obj.run()
