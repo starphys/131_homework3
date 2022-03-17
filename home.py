1945ee7b4d870c5c8d59e8658be20c3ef44233be
Evan Astle <starphys@gmail.com>
2022-03-17 15:35:00 -0700

    Google link working

--- home.py
+++ home.py
@@ -7,7 +7,8 @@ myapp_obj = Flask(__name__)
 def home():
     name = "Lisa"
     city_names = ["Paris", "London", "Rome", "Tahiti"]
-    return f"""<h1>Welcome {name}</h1>"""
+    return f"""<h1>Welcome {name}!</h1>
+        <a href="https://www.google.com">not google</a>"""
 
 
 myapp_obj.run()
