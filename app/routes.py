abba09a18d5607cad93908a1b857440506d8676c
evanastle <starphys@gmail.com>
2022-03-19 15:02:44 -0700

    Changes to align with autograder

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,11 @@
 from flask import render_template, request, flash
 from app import app
 
+name = "Lisa"
+city_names = ["Paris", "London", "Rome", "Tahiti"]
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("", methods=["GET", "POST"])
 def home():
-    name = "Lisa"
-    city_names = ["Paris", "London", "Rome", "Tahiti"]
-
     if request.method == "GET":
         return render_template("home.html", name=name, city_names=city_names)
 
--- home.py
+++ home.py
@@ -1,14 +1,13 @@
 from flask import Flask
 
 myapp_obj = Flask(__name__)
-
+name = "Lisa"
+city_names = ["Paris", "London", "Rome", "Tahiti"]
 
 @myapp_obj.route("/")
 def home():
-    name = "Lisa"
-    city_names = ["Paris", "London", "Rome", "Tahiti"]
     return_string = f"""<h1>Welcome {name}!</h1>
-        <a href="https://www.google.com">not google</a>
+        <a href="www.google.com">not google</a>
         <ul>"""
     for city in city_names:
         return_string += f"<li>{city}</li>"
